c351b4744dd78820c07c3741ac6de2ed6866d977
Matt Fenelon <matt@fnln.me>
2014-01-22 15:19:09 +0000

    Extracted the BST implementation into its own file for clarity.

---
+++ src/binarysearchtree/binarysearchtree.go
@@ -0,0 +1,74 @@
+package binarysearchtree
+
+type BinarySearchTree struct {
+	rootNode *BinarySearchTreeNode
+}
+
+type BinarySearchTreeNode struct {
+	key       string
+	value     interface{}
+	leftNode  *BinarySearchTreeNode
+	rightNode *BinarySearchTreeNode
+}
+
+func NewBinarySearchTree() *BinarySearchTree {
+	return &BinarySearchTree{
+		rootNode: nil,
+	}
+}
+
+func (bst *BinarySearchTree) Get(key string) interface{} {
+	return findValueForKey(bst.rootNode, key)
+}
+
+func (bst *BinarySearchTree) Set(key string, value interface{}) {
+	if bst.rootNode == nil {
+		bst.rootNode = &BinarySearchTreeNode{key: key, value: value}
+		return
+	}
+
+	n := findNodeForKey(bst.rootNode, key)
+	n.value = value
+}
+
+func findValueForKey(current *BinarySearchTreeNode, key string) interface{} {
+	if current == nil {
+		return nil
+	}
+
+	if current.key == key {
+		return current.value
+	}
+
+	if key > current.key {
+		return findValueForKey(current.rightNode, key)
+	}
+
+	return findValueForKey(current.leftNode, key)
+}
+
+func findNodeForKey(current *BinarySearchTreeNode, key string) *BinarySearchTreeNode {
+	if key == current.key {
+		return current
+	}
+
+	if key > current.key {
+		if current.rightNode == nil {
+			current.rightNode = &BinarySearchTreeNode{key: key}
+			return current.rightNode
+		}
+
+		return findNodeForKey(current.rightNode, key)
+	}
+
+	if key < current.key {
+		if current.leftNode == nil {
+			current.leftNode = &BinarySearchTreeNode{key: key}
+			return current.leftNode
+		}
+
+		return findNodeForKey(current.leftNode, key)
+	}
+
+	return nil
+}
